22cae39baf22e5c78889699d2b4e5a2750a3a9bc
meghna1212 <63409349+meghna1212@users.noreply.github.com>
2026-06-13 19:45:27 +0530

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+
+        unordered_map<int,int>mpp;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mpp.find(nums[i])!=mpp.end())
+                return true;
+            mpp[nums[i]]=i;
+        }
+        return false;
+
+    }
+};
