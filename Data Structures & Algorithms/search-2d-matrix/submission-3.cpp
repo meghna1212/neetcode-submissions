e488ea5e40acb576c70410328e0032ec69df12c2
meghna1212 <63409349+meghna1212@users.noreply.github.com>
2026-06-20 16:30:59 +0530

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+
+        //Find the row
+        //Smallest number, bigger than target in column-n
+        int ans_row = -1;
+        int low=0,high=rows-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            if(matrix[mid][cols-1]==target)
+                return true;
+            else if(matrix[mid][cols-1]<target)
+                low=mid+1;
+            else
+            {
+                ans_row = mid;
+                high=mid-1;
+            }
+        }
+
+        if(ans_row==-1)
+            return false;
+        //else ans_row = ans_row-1;
+
+        int low1=0,high1=cols-1;
+        while(low1<=high1)
+        {
+            int mid=(low1+high1)/2;
+
+            if(matrix[ans_row][mid]==target)
+                return true;
+            else if(matrix[ans_row][mid]<target)
+                low1=mid+1;
+            else
+                high1=mid-1;
+        }
+        return false;
+        
+    }
+};
