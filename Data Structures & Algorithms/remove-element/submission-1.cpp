e82656fbb6497ecbc0a7933121126a45bedc02de
meghna1212 <63409349+meghna1212@users.noreply.github.com>
2026-06-21 13:43:20 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int low=0,high=nums.size()-1;
+
+        while(low<=high)
+        {
+            if(nums[low]==val)
+            {
+                swap(nums[low],nums[high]);
+                high--;
+            }
+            else low++;
+        }
+        return low;
+        
+    }
+};
