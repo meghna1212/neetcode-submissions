25ad9830bce4221fbfa9a1f18dcbe1d967ae4cd0
Meghna Rai <63409349+meghna1212@users.noreply.github.com>
2026-09-12 09:58:05 -0400

    Add: is-palindrome - submission-12

---
+++ Data Structures & Algorithms/is-palindrome/submission-12.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int low=0,high=s.length()-1;
+
+        while(low<high)
+        {
+            while( low<high && ! ((s[low]>='A'&&s[low]<='Z') || (s[low]>='a'&&s[low]<='z') || (s[low]>='0'&&s[low]<='9')))
+                low++;
+            while(low<high && ! ((s[high]>='A'&&s[high]<='Z') || (s[high]>='a'&&s[high]<='z') || (s[high]>='0'&&s[high]<='9')))
+                high--;
+            
+            if(tolower(s[low])!=tolower(s[high]))
+                return false;
+
+            low++;
+            high--;
+        }
+        return true;
+        
+    }
+};
